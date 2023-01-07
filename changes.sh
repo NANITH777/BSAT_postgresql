f29e81be09b6caf596b77233b292edd6ea6dc18c
NANITH777 <nanifulchany@gmail.com>
2023-01-07 14:29:33 +0300

    script and service

---
+++ changes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir="/home/nanith/odev/test/BSAT_postgresql"
+
+database="mydata"
+table="mychanges"
+
+
+inotifywait -m -e create,delete,modify --format '%w%f %e' "$dir" | while read file event; do
+
+  if [ "$event" = "DELETE" ]; then
+    psql -c "UPDATE $table SET deleted_date=CURRENT_TIMESTAMP WHERE filename='$file'" $database
+
+  else
+    psql -c "INSERT INTO $table (date, filename) VALUES (CURRENT_TIMESTAMP, '$file')" $database
+  fi
+done
